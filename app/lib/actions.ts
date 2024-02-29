1c798345f02123dd898432cd2b1a72a30a26f326
Shenhaoyi <shenhaoyi04@163.com>
2024-02-29 16:19:45 +0800

    create an invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,34 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// 校验
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// 会给这个函数起一个接口
+export async function createInvoice(formData: FormData) {
+  // TODO:校验错误处理
+  const { customerId, amount, status } = CreateInvoice.parse(
+    Object.fromEntries(formData.entries()),
+  );
+  const amountInCents = amount * 100; // 提高计算精度
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // 本次数据库操作，导致页面数据需要更新
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices'); // 给客户端重定向
+}
