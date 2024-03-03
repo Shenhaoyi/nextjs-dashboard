bf67371fabbadf9b813e149e904448c4549284a7
Shenhaoyi <shenhaoyi04@163.com>
2024-03-04 02:01:59 +0800

    authorize all

--- auth.ts
+++ auth.ts
@@ -21,18 +21,23 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user;
-        }
-        console.log('Invalid credentials');
+        // authorize all
+        const user = await getUser('user@nextmail.com');
+        if (user) return user;
         return null;
+
+        // const parsedCredentials = z
+        //   .object({ email: z.string().email(), password: z.string().min(6) })
+        //   .safeParse(credentials);
+        // if (parsedCredentials.success) {
+        //   const { email, password } = parsedCredentials.data;
+        //   const user = await getUser(email);
+        //   if (!user) return null;
+        //   const passwordsMatch = await bcrypt.compare(password, user.password);
+        //   if (passwordsMatch) return user;
+        // }
+        // console.log('Invalid credentials');
+        // return null;
       },
     }),
   ],
